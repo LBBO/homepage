128cc4145e414679b3e81296be31f4d9c2eee6df
Michael Kuckuk <mdk@kuckuk.com>
2020-07-26 21:43:19 +0200

    Add images

---
+++ src/components/Landingpage.vue
@@ -0,0 +1,13 @@
+<template>
+  $END$
+</template>
+
+<script>
+  export default {
+    name: "Landingpage",
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Photography.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="de">
+    <div>
+      <div class="intro">
+        <h1>Außerdem beschäftige ich mich sehr gerne mit der Fotografie</h1>
+      </div>
+
+      <div class="photoOverview"></div> <!-- content is created by javascript -->
+    </div>
+  </div>
+
+  <div class="en-us">
+    <div>
+      <div class="intro">
+        <h1>Another interest of mine is photography</h1>
+      </div>
+
+      <div class="photoOverview"></div> <!-- content is created by javascript -->
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "fotography",
+  }
+</script>
+
+<style scoped>
+
+</style>
